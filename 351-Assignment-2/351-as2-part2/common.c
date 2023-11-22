93a95d54fcab6118527dfca85fed7146ce726983
luo <luo@debian.luo>
2023-11-21 19:13:21 -0800

    runCommand and sleepForMs

---
+++ 351-Assignment-2/351-as2-part2/common.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "common.h"
+
+void runCommand(char *command)
+{
+    FILE *pipe = popen(command, "r");
+    char buffer[1024];
+    while (!feof(pipe) && !ferror(pipe))
+    {
+        if (fgets(buffer, sizeof(buffer), pipe) == NULL)
+            break;
+        // Uncomment below for debugging
+        // printf("--> %s", buffer);
+    }
+    int exitCode = WEXITSTATUS(pclose(pipe));
+    if (exitCode != 0)
+    {
+        perror("Unable to execute command:");
+        printf(" command: %s\n", command);
+        printf(" exit code: %d\n", exitCode);
+    }
+}
+
+void sleepForMs(long long delayInMs)
+{
+    const long long NS_PER_MS = 1000 * 1000;
+    const long long NS_PER_SECOND = 1000000000;
+    long long delayNs = delayInMs * NS_PER_MS;
+    int seconds = delayNs / NS_PER_SECOND;
+    int nanoseconds = delayNs % NS_PER_SECOND;
+    struct timespec reqDelay = {seconds, nanoseconds};
+    nanosleep(&reqDelay, (struct timespec *)NULL);
+}
---
+++ 351-Assignment-2/351-as2-part2/common.h
@@ -0,0 +1,8 @@
+// common functions and definitions
+#ifndef common_h
+#define common_h
+
+void runCommand(char *command);
+void sleepForMs(long long delayInMs);
+
+#endif // A2D
