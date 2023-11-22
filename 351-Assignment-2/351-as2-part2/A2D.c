5b39439b85201f26cfae5bf96e598dba9e4ac25c
luo <luo@debian.luo>
2023-11-21 20:01:03 -0800

    A2D functions, getVoltageReading & get Volatge

---
+++ 351-Assignment-2/351-as2-part2/A2D.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "common.h"
+#include "A2D.h"
+
+static const double A2D_VOLTAGE_REF_V 1.8
+static const int A2D_MAX_READING 4095
+#define ADC_PATH "/sys/bus/iio/devices/iio:device0/in_voltage%d_raw"
+
+int getVoltageReading(int channel) {
+    char filePath[80];
+    snprintf(filePath, sizeof(filePath), ADC_PATH, channel);
+
+    FILE *f = fopen(filePath, "r");
+    if (!f) {
+        printf("ERROR: Unable to open voltage input file for channel %d. Cape loaded?\n", channel);
+        exit(-1);
+    }
+
+    int a2dReading = 0;
+    if (fscanf(f, "%d", &a2dReading) <= 0) {
+        printf("ERROR: Unable to read values from voltage input file for channel %d.\n", channel);
+        fclose(f);
+        exit(-1);
+    }
+
+    fclose(f);
+    return a2dReading;
+}
+
+double getVoltage(int channel) {
+    int a2dReading = getVoltageReading(channel);
+    double voltage = a2dReading * A2D_VOLTAGE_REF_V / A2D_MAX_READING;
+    return voltage;
+}
---
+++ 351-Assignment-2/351-as2-part2/A2D.h
@@ -0,0 +1,8 @@
+// Analog to Digital Converter definitions
+#ifndef A2D_H  
+#define A2D_H 
+
+int getVoltageReading(int channel);
+double getVoltage(int channel);
+
+#endif // A2D
