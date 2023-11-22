a1371bb611ad3b463d4ccdc835974d4da3ee05d4
luo <luo@debian.luo>
2023-11-21 18:33:43 -0800

    includeed all header files

---
+++ 351-Assignment-2/351-as2-part2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Include all the header files
+#include "common.h"
+#include "computing_dips.h"
+#include "display.h"
+#include "Joystick_Readings.h"
+#include "sample_analysis.h"
+#include "sample_printer.h"
+#include "sampler.h"
+#include "user_button_exit.h"
+
+
+int main() {
+}
