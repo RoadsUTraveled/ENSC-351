63c880074543785b573fde56afe9f71a5493e3dd
luo <luo@debian.luo>
2023-11-26 19:52:28 -0800

    Joystick reading funciton

---
+++ 351-Assignment-2/351-as2-part2/Joystick_Readings.c
@@ -0,0 +1,18 @@
+#include "Joystick_Readings.h"
+#include "common.h"
+#include <stdlib.h>
+
+void Joystick_readXY(double *x, double *y)
+{
+    if (x == NULL || y == NULL)
+    {
+        printf("Error: NULL pointer passed to Joystick_readXY\n");
+        exit(1);
+    }
+
+    int rawX = getVoltageReading(2), rawY = getVoltageReading(3);
+
+    *x = (double)rawX / 4095.0 * 2.0 - 1.0;
+    *y = (double)rawY / 4095.0 * 2.0 - 1.0;
+    return;
+}
