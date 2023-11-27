e17a9cfacc3d2dd736a752289b699d55dd1ff12c
luo <luo@debian.luo>
2023-11-26 20:09:11 -0800

    removed sample printer from main file

--- 351-Assignment-2/351-as2-part2/main.c
+++ 351-Assignment-2/351-as2-part2/main.c
@@ -8,7 +8,6 @@
 #include "display.h"
 #include "Joystick_Readings.h"
 #include "sample_analysis.h"
-#include "sample_printer.h"
 #include "sampler.h"
 #include "user_button_exit.h"
 #include "A2D.h"
