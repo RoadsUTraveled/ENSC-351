d4a28e7705851d6abb70a1169bd5c7fd4cf8338a
luo <luo@debian.luo>
2023-11-27 17:51:03 -0800

    Swithched define syntax form c file to h file

--- 351-Assignment-2/351-as2-part2/A2D.h
+++ 351-Assignment-2/351-as2-part2/A2D.h
@@ -3,6 +3,8 @@
 #ifndef A2D_H  
 #define A2D_H 
 
+#define ADC_PATH "/sys/bus/iio/devices/iio:device0/in_voltage%d_raw"
+
 int getVoltageReading(int channel); //channel is either 2 or 3 
 double getVoltage(int channel);
 
