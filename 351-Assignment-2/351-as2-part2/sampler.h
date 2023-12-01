550510f0d212017ff840d366f189465d119e1ddb
alan-dd <lbx15625303519@gmail.com>
2023-12-01 15:10:08 -0800

    Update sampler.h

--- 351-Assignment-2/351-as2-part2/sampler.h
+++ 351-Assignment-2/351-as2-part2/sampler.h
@@ -1,11 +1,10 @@
-// sampler.h 
-// Module to sample light levels in the background (thread). 
+// sampler.h
+// Module to sample light levels in the background (thread).
 // It provides access to the raw samples and then deletes them.
-#ifndef _SAMPLER_H_ 
+#ifndef _SAMPLER_H_
 #define _SAMPLER_H_
 
-typedef struct
-{
+typedef struct {
     double sampleInV;
     long long timestampInNanoS;
 } samplerDatapoint_t;
@@ -13,11 +12,14 @@ typedef struct
 // Begin/end the background thread which samples light levels.
 void Sampler_startSampling(void);
 void Sampler_stopSampling(void);
-// Get a copy of the samples in the sample history, removing values 
-// from our history here. 
-// Returns a newly allocated array and sets `length` to be the 
-// number of elements in the returned array (output-only parameter). 
-// The calling code must call free() on the returned pointer. 
+
+void* samplingFunction(void* arg);
+
+// Get a copy of the samples in the sample history, removing values
+// from our history here.
+// Returns a newly allocated array and sets `length` to be the
+// number of elements in the returned array (output-only parameter).
+// The calling code must call free() on the returned pointer.
 // Note: function provides both data and size to ensure consistency.
 samplerDatapoint_t* Sampler_extractAllValues(int *length);
 
@@ -26,4 +28,5 @@ int Sampler_getNumSamplesInHistory();
 
 // Get the total number of light level samples taken so far.
 long long Sampler_getNumSamplesTaken(void);
+void Sampler_resetSampleCount(void);
 #endif
