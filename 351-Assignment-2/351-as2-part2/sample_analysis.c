fcc6e877ae528cc5d0da9c5f464ac1f12a2786bc
alan-dd <lbx15625303519@gmail.com>
2023-11-30 21:52:33 -0800

    Update sample_analysis.c

--- 351-Assignment-2/351-as2-part2/sample_analysis.c
+++ 351-Assignment-2/351-as2-part2/sample_analysis.c
@@ -7,10 +7,10 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <float.h>
-#include <math.h>  // Add this line
+#include <math.h> // Add this line
 
-
-typedef struct {
+typedef struct
+{
     double avgVoltage;
     double minVoltage;
     double maxVoltage;
@@ -37,95 +37,116 @@ void cleanup_sample_analysis(void)
     pthread_mutex_destroy(&sampleAnalysisMutex);
 }
 
-//Helper function for voltage
-double findMaxVoltage(samplerDatapoint_t* samples, int numSamples) {
+// Helper function for voltage
+double findMaxVoltage(samplerDatapoint_t *samples, int numSamples)
+{
     double maxVoltage = -DBL_MAX;
-    for (int i = 0; i < numSamples; ++i) {
-        if (samples[i].sampleInV > maxVoltage) {
+    for (int i = 0; i < numSamples; ++i)
+    {
+        if (samples[i].sampleInV > maxVoltage)
+        {
             maxVoltage = samples[i].sampleInV;
         }
     }
     return maxVoltage;
 }
-double findMinVoltage(samplerDatapoint_t* samples, int numSamples) {
+double findMinVoltage(samplerDatapoint_t *samples, int numSamples)
+{
     double minVoltage = DBL_MAX;
-    for (int i = 0; i < numSamples; ++i) {
-        if (samples[i].sampleInV < minVoltage) {
+    for (int i = 0; i < numSamples; ++i)
+    {
+        if (samples[i].sampleInV < minVoltage)
+        {
             minVoltage = samples[i].sampleInV;
         }
     }
     return minVoltage;
 }
-double findAvgVoltage(samplerDatapoint_t* samples, int numSamples) {
+double findAvgVoltage(samplerDatapoint_t *samples, int numSamples)
+{
     double totalVoltage = 0.0;
-    for (int i = 0; i < numSamples; ++i) {
+    for (int i = 0; i < numSamples; ++i)
+    {
         totalVoltage += samples[i].sampleInV;
     }
     return numSamples > 0 ? totalVoltage / numSamples : 0.0;
 }
 
-
-//Helper function for time Interval
-double findMaxInterval(samplerDatapoint_t* samples, int numSamples) {
+// Helper function for time Interval
+double findMaxInterval(samplerDatapoint_t *samples, int numSamples)
+{
     double maxInterval = -DBL_MAX;
-    for (int i = 1; i < numSamples; ++i) {
+    for (int i = 1; i < numSamples; ++i)
+    {
         double interval = samples[i].timestampInNanoS - samples[i - 1].timestampInNanoS;
         maxInterval = fmax(maxInterval, interval);
     }
     return maxInterval;
 }
 
-double findMinInterval(samplerDatapoint_t* samples, int numSamples) {
+double findMinInterval(samplerDatapoint_t *samples, int numSamples)
+{
     double minInterval = DBL_MAX;
-    for (int i = 1; i < numSamples; ++i) {
+    for (int i = 1; i < numSamples; ++i)
+    {
         double interval = samples[i].timestampInNanoS - samples[i - 1].timestampInNanoS;
         minInterval = fmin(minInterval, interval);
     }
     return minInterval;
 }
 
-double findAvgInterval(samplerDatapoint_t* samples, int numSamples) {
+double findAvgInterval(samplerDatapoint_t *samples, int numSamples)
+{
     double totalInterval = 0.0;
-    for (int i = 1; i < numSamples; ++i) {
+    for (int i = 1; i < numSamples; ++i)
+    {
         totalInterval += samples[i].timestampInNanoS - samples[i - 1].timestampInNanoS;
     }
     return (numSamples > 1) ? totalInterval / (numSamples - 1) : 0.0;
 }
 
-
-//printer ffuntions
-void printSampleAnalysis(void);
-
-//Update Average Voltage
-static void updateAvgVoltage(double newVoltage) {
-    if (sampleAnalysis.numSamples == 0) {
+// Update Average Voltage
+/*
+static void updateAvgVoltage(double newVoltage)
+{
+    if (sampleAnalysis.numSamples == 0)
+    {
         sampleAnalysis.avgVoltage = newVoltage;
-    } else {
+    }
+    else
+    {
         sampleAnalysis.avgVoltage = (sampleAnalysis.avgVoltage * sampleAnalysis.numSamples + newVoltage) / (sampleAnalysis.numSamples + 1);
     }
 }
+*/
 
-//Check Dips with ragard to the threshold
-static void checkDips(double newSample) {
-    if (!sampleAnalysis.dip && newSample < sampleAnalysis.avgVoltage - DIP_THRESHOLD) {
+// Check Dips with ragard to the threshold
+static void checkDips(double newSample)
+{
+    //When dip is false, it means detected
+    if (!sampleAnalysis.dip && newSample < sampleAnalysis.avgVoltage - DIP_THRESHOLD)
+    {
         sampleAnalysis.dip = true;
         sampleAnalysis.numDips++;
-    } else if (sampleAnalysis.dip && newSample > sampleAnalysis.avgVoltage - DIP_THRESHOLD + DIP_HYSTERESIS) {
+    }
+    else if (sampleAnalysis.dip && newSample > sampleAnalysis.avgVoltage - DIP_THRESHOLD + DIP_HYSTERESIS)
+    {
         sampleAnalysis.dip = false;
     }
 }
-void init_sample_analysis(void) {
+void resetSampleAnalysis(void)
+{
     pthread_mutex_init(&sampleAnalysisMutex, NULL);
     sampleAnalysisThreadActive = true;
 
     // Initialize the sampleAnalysis struct
     pthread_mutex_lock(&sampleAnalysisMutex);
     sampleAnalysis.avgVoltage = 0.0;
-    sampleAnalysis.minVoltage = 0.1;  // Set to maximum double value
-    sampleAnalysis.maxVoltage = 1.8; // Set to minimum double value
+    sampleAnalysis.minVoltage = DBL_MAX; // Set to maximum double value
+    sampleAnalysis.maxVoltage = DBL_MIN; // Set to minimum double value
     sampleAnalysis.avgInterval = 0.0;
-    sampleAnalysis.minInterval = 0.0;
-    sampleAnalysis.maxInterval = 0.1;
+    sampleAnalysis.minInterval = DBL_MAX;
+    sampleAnalysis.maxInterval = DBL_MIN;
     sampleAnalysis.numSamples = 0;
     sampleAnalysis.numDips = 0;
     sampleAnalysis.dip = false;
@@ -133,46 +154,54 @@ void init_sample_analysis(void) {
 
     pthread_create(&sampleAnalysisThread, NULL, sampleAnalysisThreadFunc, NULL);
 }
+void computeStatistics(samplerDatapoint_t* samples, int length) 
+{
+    resetSampleAnalysis();
 
-//Analysis of the sample in multiple threads
-void* sampleAnalysisThreadFunc(void* arg) {
-    
-    while (sampleAnalysisThreadActive) {
+    sampleAnalysis.avgVoltage = findAvgVoltage(samples, length);
+    sampleAnalysis.minVoltage = findMinVoltage(samples, length);
+    sampleAnalysis.maxVoltage = findMaxVoltage(samples, length);
+
+    sampleAnalysis.avgInterval = findAvgInterval(samples, length);
+    sampleAnalysis.minInterval = findMinInterval(samples, length);
+    sampleAnalysis.maxInterval = findMaxInterval(samples, length);
+
+    for (int i = 0; i < length; ++i) {
+        checkDips(samples[i].sampleInV);
+    }
+
+    sampleAnalysis.numSamples = length;
+}
+
+
+// Analysis of the sample in multiple threads
+void *sampleAnalysisThreadFunc(void *arg)
+{
+    while (sampleAnalysisThreadActive)
+    {
         sleep(1);
-        pthread_mutex_lock(&sampleAnalysisMutex);
 
         // Assuming Sampler_extractAllValues function exists to get all samples
         int numSamples;
-        samplerDatapoint_t* samples = Sampler_extractAllValues(&numSamples);
+        samplerDatapoint_t *samples = Sampler_extractAllValues(&numSamples);
 
-        // Compute statistics
-        sampleAnalysis.minVoltage = findMinVoltage(samples, numSamples);
-        sampleAnalysis.maxVoltage = findMaxVoltage(samples, numSamples);
-        sampleAnalysis.avgVoltage = findAvgVoltage(samples, numSamples);
-        sampleAnalysis.minInterval = findMinInterval(samples, numSamples);
-        sampleAnalysis.maxInterval = findMaxInterval(samples, numSamples);
-        sampleAnalysis.avgInterval = findAvgInterval(samples, numSamples);
-        sampleAnalysis.numSamples = numSamples;
-
-        // Check for dips
-        for (int i = 0; i < numSamples; ++i) {
-            updateAvgVoltage(samples[i].sampleInV);
-            checkDips(samples[i].sampleInV);
+        if(numSamples > 0){
+            pthread_mutex_lock(&sampleAnalysisMutex);
+            computeStatistics(samples,numSamples);
+            pthread_mutex_unlock(&sampleAnalysisMutex);
+            printSampleAnalysis();
+            printf("\n");
         }
-
         // Reset sample Count for the next second
-        numSamples = 0;
-        
-        pthread_mutex_unlock(&sampleAnalysisMutex);
+        Sampler_resetSampleCount();
 
-        // Print the analysis
-        printSampleAnalysis();
-
+        free(samples);
     }
     return NULL;
 }
 
-void printSampleAnalysis(void) {
+void printSampleAnalysis(void)
+{
     pthread_mutex_lock(&sampleAnalysisMutex);
 
     printf("Average Voltage: %.3f V\n", sampleAnalysis.avgVoltage);
@@ -186,4 +215,3 @@ void printSampleAnalysis(void) {
 
     pthread_mutex_unlock(&sampleAnalysisMutex);
 }
-
