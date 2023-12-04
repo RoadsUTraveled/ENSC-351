2e5608c023e8a607bb7e94298270dc22f954417e
luo <luo@debian.luo>
2023-12-03 22:25:43 -0800

    Common function to setup GPIO pins

---
+++ 351-Project/RemoteCart/common.c
@@ -0,0 +1,94 @@
+#include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+// Path: common.h
+
+void exportGPIO(int gpio)
+{
+    int fd = open(GPIO_EXPORT, O_WRONLY);
+    if (fd == -1)
+    {
+        perror("Unable to open /sys/class/gpio/export");
+        exit(1);
+    }
+
+    char buffer[3];
+    sprintf(buffer, "%d", gpio);
+    if (write(fd, buffer, strlen(buffer)) != strlen(buffer))
+    {
+        perror("Error writing to /sys/class/gpio/export");
+        exit(1);
+    }
+
+    close(fd);
+}
+
+void unexportGPIO(int gpio)
+{
+    int fd = open(GPIO_UNEXPORT, O_WRONLY);
+    if (fd == -1)
+    {
+        perror("Unable to open /sys/class/gpio/unexport");
+        exit(1);
+    }
+
+    char buffer[3];
+    sprintf(buffer, "%d", gpio);
+    if (write(fd, buffer, strlen(buffer)) != strlen(buffer))
+    {
+        perror("Error writing to /sys/class/gpio/unexport");
+        exit(1);
+    }
+
+    close(fd);
+}
+
+void setGPIO_Direction(int gpio, char *direction)
+{
+    char path[35];
+    sprintf(path, GPIO_DIRECTION, gpio);
+
+    int fd = open(path, O_WRONLY);
+    if (fd == -1)
+    {
+        perror("Unable to open GPIO direction file");
+        exit(1);
+    }
+
+    if (write(fd, direction, strlen(direction)) != strlen(direction))
+    {
+        perror("Error writing to GPIO direction file");
+        exit(1);
+    }
+
+    close(fd);
+}
+
+void setGPIO_Value(int gpio, int value)
+{
+    char path[30];
+    sprintf(path, GPIO_VALUE, gpio);
+
+    int fd = open(path, O_WRONLY);
+    if (fd == -1)
+    {
+        perror("Unable to open GPIO value file");
+        exit(1);
+    }
+
+    char buffer[2];
+    sprintf(buffer, "%d", value);
+    if (write(fd, buffer, 1) != 1)
+    {
+        perror("Error writing to GPIO value file");
+        exit(1);
+    }
+
+    close(fd);
+}
--- 351-Project/RemoteCart/common.h
+++ 351-Project/RemoteCart/common.h
@@ -1,4 +1,19 @@
+// Description: Common functions
+// exportGPIO() - exports a GPIO
+// unexportGPIO() - unexports a GPIO
+// setGPIO_Direction() - sets the direction of a GPIO
+// setGPIO_Value() - sets the value of a GPIO
 #ifndef COMMON_H
 #define COMMON_H
+
+#define GPIO_EXPORT "/sys/class/gpio/export"
+#define GPIO_UNEXPORT "/sys/class/gpio/unexport"
+#define GPIO_DIRECTION "/sys/class/gpio/gpio%d/direction"
+#define GPIO_VALUE "/sys/class/gpio/gpio%d/value"
+
+void exportGPIO(int gpio);
+void unexportGPIO(int gpio);
+void setGPIO_Direction(int gpio, char* direction);
+void setGPIO_Value(int gpio, int value);
 
 #endif // COMMON_H
